0a5397969842b8e5fa9edb2350dc117b02739c4e
OlegSimfi <olegsimfi@gmail.com>
2018-12-06 13:04:00 +0200

    Add Data types

---
+++ index.js
@@ -0,0 +1,12 @@
+// string
+var str = 'string';
+//number
+var num = 100;
+// boolean
+var isTrue = true;
+var isFalse = false;
+//other
+var uknown = '1';
+uknown = 1;
+var uknown2 = '1';
+uknown2 = 1;
---
+++ index.ts
@@ -0,0 +1,18 @@
+// string
+let str: string = 'string';
+
+//number
+let num: number = 100;
+
+// boolean
+let isTrue = true;
+let isFalse = false;
+
+//other
+let uknown: any = '1';
+uknown = 1;
+
+let uknown2: string | number = '1';
+uknown2 = 1;
+
+
